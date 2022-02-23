e3c3188e5c390a28959cdfd5ce048383e813fafe
tejal199 <tejal1999jagtap@gmail.com>
2022-02-23 11:00:07 +0000

    Maven Second  project by using command line

---
+++ src/main/java/com/tejal/App.java
@@ -0,0 +1,16 @@
+package com.tejal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+    	 System.out.println( "Sum is " + sum );
+    }
+}
